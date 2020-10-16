587fcab9d3b8823bc4d70a42c60baa2410199a72
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2020-10-16 13:31:53 +0300

    Add skeleton for unit tests of the scene

---
+++ src/scene.test.ts
@@ -0,0 +1,43 @@
+describe("draw", () => {
+  it("saves drawing to shapes", () => {
+    // TODO
+  });
+
+  it("after undo resets further history", () => {
+    // TODO
+  });
+});
+
+describe("erase", () => {
+  it("erase marks shape as pink and then invisible", () => {
+    // TODO
+  });
+});
+
+describe("undo", () => {
+  it("sets shape as not visible", () => {
+    // TODO
+  });
+
+  it("while drawing resets the drawn shape without changing the history", () => {
+    // TODO
+  });
+
+  it("after erasing two shapes restores both of them", () => {
+    // TODO
+  });
+
+  it("of an erase restores shape", () => {
+    // TODO
+  });
+
+  it("while erasing leaves the shape intact also without changing the history", () => {
+    // TODO
+  });
+});
+
+describe("redo", () => {
+  it("sets shape as visible", () => {
+    // TODO
+  });
+});
