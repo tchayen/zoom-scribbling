12b5d0b96e5d1ed30f272a9379a0d1a0c34c7790
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2020-10-20 01:44:08 +0300

    Add toggle button

---
+++ src/components/ToggleButton.tsx
@@ -0,0 +1,94 @@
+import React, { createContext, useContext, useRef } from "react";
+import { RadioGroupState, useRadioGroupState } from "@react-stately/radio";
+import { AriaRadioGroupProps } from "@react-types/radio";
+import { useFocusRing } from "@react-aria/focus";
+import { useRadio, useRadioGroup } from "@react-aria/radio";
+import { VisuallyHidden } from "@react-aria/visually-hidden";
+import { mergeProps } from "@react-aria/utils";
+import { styled } from "./colorTheme";
+import Label from "./Label";
+
+const ToggleContext = createContext<{
+  state: RadioGroupState;
+  isDisabled: boolean;
+} | null>(null);
+
+const ToggleButtonComponent = styled.div<{
+  isFocusVisible: boolean;
+  isSelected: boolean;
+}>`
+  -webkit-appearance: none;
+  outline: none;
+  background-color: ${(props) =>
+    props.isSelected ? props.theme.primaryDimmed : "transparent"};
+  box-shadow: ${(props) =>
+    props.isFocusVisible ? `0 0 0 4px ${props.theme.primaryDimmed}` : "none"};
+`;
+
+const Row = styled.label<{ isDisabled: boolean }>`
+  font-size: 14px;
+  display: flex;
+  align-items: center;
+  height: 24px;
+  opacity: ${(props) => (props.isDisabled ? 0.5 : 1)};
+`;
+
+type Props = {
+  Icon: any;
+  value: string;
+};
+
+export const ToggleButton = (props: Props) => {
+  const context = useContext(ToggleContext);
+
+  const ref = useRef(null);
+  const { inputProps } = useRadio(
+    props,
+    context!.state as RadioGroupState,
+    ref
+  );
+  const { isFocusVisible, focusProps } = useFocusRing();
+
+  if (context === null) {
+    return null;
+  }
+
+  return (
+    <Row isDisabled={context.isDisabled}>
+      <VisuallyHidden>
+        <input
+          {...mergeProps(inputProps, focusProps)}
+          disabled={context.isDisabled}
+          ref={ref}
+        />
+      </VisuallyHidden>
+      <ToggleButtonComponent
+        isFocusVisible={isFocusVisible}
+        isSelected={context.state.selectedValue === inputProps.value}
+      >
+        <props.Icon color="black" />
+      </ToggleButtonComponent>
+    </Row>
+  );
+};
+
+type ToggleButtonGroupProps = {
+  children: React.ReactNode;
+} & AriaRadioGroupProps;
+
+export const ToggleButtonGroup = (props: ToggleButtonGroupProps) => {
+  const { children, label } = props;
+  const state = useRadioGroupState(props);
+  const { radioGroupProps, labelProps } = useRadioGroup(props, state);
+
+  return (
+    <div {...radioGroupProps}>
+      <Label {...labelProps} isDisabled={props.isDisabled}>
+        {label}
+      </Label>
+      <ToggleContext.Provider value={{ state, isDisabled: !!props.isDisabled }}>
+        {children}
+      </ToggleContext.Provider>
+    </div>
+  );
+};
