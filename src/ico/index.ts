bf4342db838a00c61ee07ed4de08cee0e7eccf99
Tomasz Czajęcki <tomek.czajecki@gmail.com>
2020-10-20 01:44:35 +0300

    Add index.ts to icons

---
+++ src/ico/index.ts
@@ -0,0 +1,12 @@
+export { default as Copy } from "./Copy";
+export { default as Erase } from "./Erase";
+export { default as Folder } from "./Folder";
+export { default as Import } from "./Import";
+export { default as Moon } from "./Moon";
+export { default as New } from "./New";
+export { default as Pencil } from "./Pencil";
+export { default as Picture } from "./Picture";
+export { default as Remove } from "./Remove";
+export { default as Save } from "./Save";
+export { default as Selection } from "./Selection";
+export { default as Sun } from "./Sun";
